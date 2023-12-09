a5d3b808f871c7e15c675af0090b509df319731d
obiwac <obiwac@gmail.com>
2023-12-09 19:55:17 +0100

    project: Node objects

--- project/index.js
+++ project/index.js
@@ -1,3 +1,5 @@
+const TAU = 2 * Math.PI
+
 // initialize WebGL context
 
 const canvas = document.getElementById("canvas")
@@ -14,6 +16,62 @@ const x_res = gl.drawingBufferWidth
 const y_res = gl.drawingBufferHeight
 
 gl.viewport(0, 0, x_res, y_res)
+
+// nodes
+
+class Node {
+	constructor() {
+		const RADIUS = 0.5
+		const DETAIL = 32
+
+		let vertices = []
+		let indices = []
+
+		// centre vertex all triangles will fan out from
+
+		vertices.push(0, 0)
+
+		// generate surrounding vertices
+
+		for (let i = 0; i < DETAIL; i++) {
+			const angle = (i / DETAIL) * TAU
+			vertices.push(Math.cos(angle) * RADIUS, Math.sin(angle) * RADIUS)
+		}
+
+		// generate indices for all triangles
+
+		for (let i = 0; i < DETAIL - 1; i++) {
+			indices.push(0, i ? DETAIL : i + 1, i + 2)
+		}
+
+		/** @type: Float32Array */
+		this.vbo_data = new Float32Array(vertices)
+
+		/** @type: Uint8Array */
+		this.indices = new Uint8Array(indices)
+
+		this.vao = gl.createVertexArray()
+		gl.bindVertexArray(this.vao)
+
+		this.vbo = gl.createBuffer()
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo)
+		gl.bufferData(gl.ARRAY_BUFFER, this.vbo_data, gl.STATIC_DRAW)
+
+		gl.enableVertexAttribArray(0)
+		gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0)
+
+		this.ibo = gl.createBuffer()
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ibo)
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW)
+	}
+
+	draw() {
+		gl.bindVertexArray(this.vao)
+		gl.drawElements(gl.TRIANGLES, this.indices.length, gl.UNSIGNED_BYTE, 0)
+	}
+}
+
+const node = new Node()
 
 // rendering
 
