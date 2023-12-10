7e6d06ebd3a62c452d40b9207f90a956d2929695
obiwac <obiwac@gmail.com>
2023-12-10 23:59:11 +0100

    project: Nodes class which has VBO/IBO for all nodes

--- project/index.js
+++ project/index.js
@@ -23,58 +23,92 @@ gl.viewport(0, 0, x_res, y_res)
 // nodes
 
 class Node {
-	constructor() {
-		const RADIUS = 0.02
-		const DETAIL = 32
+	constructor(x, y) {
+		this.x = x
+		this.y = y
+	}
+}
 
-		let vertices = []
-		let indices = []
+class Nodes {
+	constructor(nodeData) {
+		this.nodes = []
 
-		// centre vertex all triangles will fan out from
-
-		vertices.push(0, 0)
-
-		// generate surrounding vertices
-
-		for (let i = 0; i < DETAIL; i++) {
-			const angle = (i / DETAIL) * TAU
-			vertices.push(Math.cos(angle) * RADIUS, Math.sin(angle) * RADIUS)
+		for (const [x, y] of nodeData) {
+			this.nodes.push(new Node(x, y))
 		}
-
-		// generate indices for all triangles
-
-		for (let i = 0; i < DETAIL; i++) {
-			indices.push(0, i + 1, i ? i : DETAIL)
-		}
-
-		/** @type: Float32Array */
-		this.vbo_data = new Float32Array(vertices)
-
-		/** @type: Uint8Array */
-		this.indices = new Uint8Array(indices)
 
 		this.vao = gl.createVertexArray()
 		gl.bindVertexArray(this.vao)
 
 		this.vbo = gl.createBuffer()
 		gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo)
-		gl.bufferData(gl.ARRAY_BUFFER, this.vbo_data, gl.STATIC_DRAW)
 
 		gl.enableVertexAttribArray(0)
 		gl.vertexAttribPointer(0, 2, gl.FLOAT, false, FLOAT32_SIZE * 2, 0)
 
 		this.ibo = gl.createBuffer()
 		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ibo)
-		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW)
+	}
+
+	update_mesh() {
+		// create mesh data
+
+		const RADIUS = 0.02
+		const DETAIL = 32
+
+		let vertices = []
+		let indices = []
+
+		for (const node of this.nodes) {
+			// centre vertex all triangles will fan out from
+
+			vertices.push(node.x, node.y)
+
+			// generate surrounding vertices
+
+			for (let i = 0; i < DETAIL; i++) {
+				const angle = (i / DETAIL) * TAU
+				vertices.push(Math.cos(angle) * RADIUS + node.x, Math.sin(angle) * RADIUS + node.y)
+			}
+
+			// generate indices for all triangles
+
+			const off = vertices.length
+
+			for (let i = 0; i < DETAIL; i++) {
+				indices.push(off, off + i + 1, off + (i ? i : DETAIL))
+			}
+		}
+
+		this.indices_length = indices.length
+
+		/** @type: Float32Array */
+		const vbo_data = new Float32Array(vertices)
+
+		/** @type: Uint32Array */
+		const ibo_data = new Uint32Array(indices)
+
+		// upload mesh data to GPU
+
+		gl.bindVertexArray(this.vao)
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo)
+		gl.bufferData(gl.ARRAY_BUFFER, vbo_data, gl.STATIC_DRAW)
+
+		gl.enableVertexAttribArray(0)
+		gl.vertexAttribPointer(0, 2, gl.FLOAT, false, FLOAT32_SIZE * 2, 0)
+
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ibo)
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, ibo_data, gl.STATIC_DRAW)
 	}
 
 	draw() {
 		gl.bindVertexArray(this.vao)
-		gl.drawElements(gl.TRIANGLES, this.indices.length, gl.UNSIGNED_BYTE, 0)
+		gl.drawElements(gl.TRIANGLES, this.indices_length, gl.UNSIGNED_INT, 0)
 	}
 }
 
-const node = new Node()
+const nodes = new Nodes(nodeData)
 const node_shader = new Shader("node")
 
 // camera controls
@@ -156,13 +190,10 @@ function render(now) {
 	// render nodes
 
 	node_shader.use()
+	node_shader.mvp(mvp_mat)
 
-	for (const [x, y] of nodeData) {
-		mvp_mat.translate(x, y, 0)
-		node_shader.mvp(mvp_mat)
-		node.draw()
-		mvp_mat.translate(-x, -y, 0)
-	}
+	nodes.update_mesh()
+	nodes.draw()
 
 	// continue render loop
 
