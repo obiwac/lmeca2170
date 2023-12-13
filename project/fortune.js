865d93a43ca71100e091e6439fafc5004847e19b
obiwac <obiwac@gmail.com>
2023-12-13 19:42:49 +0100

    project: Make fortune source file follow proper indentation style

--- project/fortune.js
+++ project/fortune.js
@@ -1,19 +1,19 @@
 class IncompleteEdge {
-    constructor(p1, p2, startx) {
-        this.slope = (p1.y - p2.y) / (p1.x - p2.x)
+	constructor(p1, p2, startx) {
+		this.slope = (p1.y - p2.y) / (p1.x - p2.x)
 
-        this.end = null
-        this.start = null
-    }
+		this.end = null
+		this.start = null
+	}
 }
 
 function get_y(p, x, directrix) {
-    const dp = 2 * (p.y - directrix)
-    const a1 = 1 / dp
-    const b1 = -2 * p.x / dp
-    const c1 = directrix + dp / 4 + p.x * p.x / dp
+	const dp = 2 * (p.y - directrix)
+	const a1 = 1 / dp
+	const b1 = -2 * p.x / dp
+	const c1 = directrix + dp / 4 + p.x * p.x / dp
 
-    return a1 * x * x + b1 * x + c1
+	return a1 * x * x + b1 * x + c1
 }
 
 /** @function
@@ -21,63 +21,65 @@ function get_y(p, x, directrix) {
   * @returns {Triangle[]}
   */
 function fortune(nodes) {
-    const events = {
-        "site": 0,
-        "circle": 1,
-        "parabola": 2,
-        "skip": 3,
-    }
+	const events = {
+		"site": 0,
+		"circle": 1,
+		"parabola": 2,
+		"skip": 3,
+	}
 
-    let queue = []
+	let queue = []
 
-    for (const node of nodes) {
-        queue.push([node, events.site])
-    }
+	for (const node of nodes) {
+		queue.push([node, events.site])
+	}
 
-    let head = queue.length - 1
+	let head = queue.length - 1
 
-    queue.sort((a, b) => a[0].y - b[0].y)
-    const beachline = new BeachTree()
+	queue.sort((a, b) => a[0].y - b[0].y)
+	const beachline = new BeachTree()
 
-    while (head >= 0) {
-        current_event = queue[head--]
+	while (head >= 0) {
+		current_event = queue[head--]
 
-        if (current_event[1] == events.site) {            
-            
-            if(beachline.root == null) {
-                beachline.root = new BeachNode(current_event[0])
-                continue
-            }
-            // On cherche l'arc qui est au dessus du point
+		if (current_event[1] == events.site) {			
+			
+			if(beachline.root == null) {
+				beachline.root = new BeachNode(current_event[0])
+				continue
+			}
+			// On cherche l'arc qui est au dessus du point
 
-            let above_arc = beachline.find_arc_above(current_event[0])
-            console.log("above arc", above_arc.site)
+			let above_arc = beachline.find_arc_above(current_event[0])
+			console.log("above arc", above_arc.site)
 
-            let left_arc = new BeachNode(above_arc.site)
-            let middle_arc = new BeachNode(current_event[0])
-            let right_arc = new BeachNode(above_arc.site)
+			let left_arc = new BeachNode(above_arc.site)
+			let middle_arc = new BeachNode(current_event[0])
+			let right_arc = new BeachNode(above_arc.site)
 
-            let start_point = new Point(current_event[0], get_y(current_event[0], above_arc.site.x, current_event[0].y))
-            
-            let edge_left = new Edge(start_point, above_arc.site, current_event[0])
-            let edge_right = new Edge(start_point, current_event[0], above_arc.site)
+			let start_point = new Point(current_event[0], get_y(current_event[0], above_arc.site.x, current_event[0].y))
+			
+			let edge_left = new Edge(start_point, above_arc.site, current_event[0])
+			let edge_right = new Edge(start_point, current_event[0], above_arc.site)
 
-            edge_left.set_parent(above_arc)  
-            edge_left.set_left(left_arc)
-            edge_left.set_right(edge_right)
-            
-            edge_right.set_left(middle_arc)
-            edge_right.set_right(right_arc)
+			edge_left.set_parent(above_arc)
+			edge_left.set_left(left_arc)
+			edge_left.set_right(edge_right)
+			
+			edge_right.set_left(middle_arc)
+			edge_right.set_right(right_arc)
 
-            // Check if the root was the above arc, this souls occurs only once because after all arcs are leafs.
-            if (beachline.root == above_arc) {
-                console.log("L'arc au dessu est le root")
-                beachline.root = edge_left
-            }
+			// Check if the root was the above arc, this souls occurs only once because after all arcs are leafs.
+			if (beachline.root == above_arc) {
+				console.log("L'arc au dessu est le root")
+				beachline.root = edge_left
+			}
 
 
-        } else if (current_event.type == events.circle) {
-            // TODO
-        }
-    }
+		} else if (current_event.type == events.circle) {
+			// TODO
+		}
+	}
+
+	return []
 }
