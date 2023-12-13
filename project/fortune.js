ed067c33b52a2e34779a7f56b021bc9bf49ee36a
obiwac <obiwac@gmail.com>
2023-12-13 19:56:33 +0100

    project: Format shit correctly

--- project/fortune.js
+++ project/fortune.js
@@ -42,30 +42,30 @@ function fortune(nodes) {
 	while (head >= 0) {
 		current_event = queue[head--]
 
-		if (current_event[1] == events.site) {			
-			
+		if (current_event[1] == events.site) {
 			if(beachline.root == null) {
 				beachline.root = new BeachNode(current_event[0])
 				continue
 			}
+
 			// On cherche l'arc qui est au dessus du point
 
-			let above_arc = beachline.find_arc_above(current_event[0])
+			const above_arc = beachline.find_arc_above(current_event[0])
 			console.log("above arc", above_arc.site)
 
-			let left_arc = new BeachNode(above_arc.site)
-			let middle_arc = new BeachNode(current_event[0])
-			let right_arc = new BeachNode(above_arc.site)
+			const left_arc = new BeachNode(above_arc.site)
+			const middle_arc = new BeachNode(current_event[0])
+			const right_arc = new BeachNode(above_arc.site)
 
-			let start_point = new Point(current_event[0], get_y(current_event[0], above_arc.site.x, current_event[0].y))
-			
-			let edge_left = new Edge(start_point, above_arc.site, current_event[0])
-			let edge_right = new Edge(start_point, current_event[0], above_arc.site)
+			const start_point = new Point(current_event[0], get_y(current_event[0], above_arc.site.x, current_event[0].y))
+
+			const edge_left = new Edge(start_point, above_arc.site, current_event[0])
+			const edge_right = new Edge(start_point, current_event[0], above_arc.site)
 
 			edge_left.set_parent(above_arc)
 			edge_left.set_left(left_arc)
 			edge_left.set_right(edge_right)
-			
+
 			edge_right.set_left(middle_arc)
 			edge_right.set_right(right_arc)
 
@@ -74,9 +74,9 @@ function fortune(nodes) {
 				console.log("L'arc au dessu est le root")
 				beachline.root = edge_left
 			}
+		}
 
-
-		} else if (current_event.type == events.circle) {
+		else if (current_event.type == events.circle) {
 			// TODO
 		}
 	}
--- project/red-black-tree.js
+++ project/red-black-tree.js
@@ -5,7 +5,7 @@
 
 
 /*
-* Structure un peu spéciale, c'est un binary search tree mais les leafs sont les arcs 
+* Structure un peu spéciale, c'est un binary search tree mais les leafs sont les arcs
 * et les noeuds internes sont les edges.
 * https://blog.ivank.net/fortunes-algorithm-and-implementation.html
 */
@@ -19,40 +19,32 @@ class BeachNode {
 		this.parent = null
 		this.edge = null
 
-		this.circleEvent = null
+		this.circle_event = null
 		this.is_leaf = true
-
 	}
 
-	/**
-	 * 
-	 * @param {BeachNode} node
-	 *  set the left child of the current node to node 
-	 */
+	/** @function set the left child of the current node to node
+	  * @param {BeachNode} node
+	  */
 	set_left(node) {
 		this.left = node
 		this.is_leaf = false
 		node.parent = this
 	}
 
-	/**
-	 * 
-	 * @param {BeachNode} node
-	 *  set the right child of the current node to node
-	 */
+	/** @function set the right child of the current node to node
+	  * @param {BeachNode} node
+	  */
 	set_right(node) {
 		this.right = node
 		this.is_leaf = false
 		node.parent = this
 	}
 
-	/**
-	 * 
-	 * @param {BeachNode} node the parent node
-	 */
-
+	/** @function
+	  * @param {BeachNode} node the parent node
+	  */
 	set_parent(node) {
-		
 		console.log("set parent called with: ", node, " to ", this)
 
 		// Ça arrive quand c'est la première feuille dans l'abre (quand la feuille est la racine)
@@ -63,7 +55,9 @@ class BeachNode {
 
 		if(node.parent.left == node) {
 			node.parent.set_left(this)
-		} else {
+		}
+
+		else {
 			node.parent.set_right(this)
 		}
 	}
@@ -72,7 +66,6 @@ class BeachNode {
 
 class Edge extends BeachNode {
 	constructor(start, left, right) {
-		
 		super(null)
 
 		this.start = start
@@ -80,7 +73,7 @@ class Edge extends BeachNode {
 		this.right = right
 
 		this.end = null
-		this.neighbour = null // this is an edge	
+		this.neighbour = null // this is an edge
 		this.is_leaf = true
 
 		this.slope = (left.x - right.x) / (right.y - left.y)
@@ -96,20 +89,21 @@ class BeachTree {
 
 	get_first_parent_on_left(node) {
 		let current_node = node
-		
+
 		while (current_node.parent != null && current_node.parent.left == current_node) {
 			current_node = current_node.parent
 		}
-		
+
 		return current_node.parent
 	}
 
 	get_first_parent_on_right(node) {
 		let current_node = node
+
 		while (current_node.parent != null && current_node.parent.right == current_node) {
 			current_node = current_node.parent
 		}
-				
+
 		return current_node.parent
 	}
 
@@ -117,9 +111,9 @@ class BeachTree {
 		if (node.left == null) {
 			return null
 		}
-		
+
 		let current_node = node.left
-		
+
 		while (current_node.is_leaf == false) {
 			current_node = current_node.right
 		}
@@ -131,24 +125,25 @@ class BeachTree {
 		if (node.right == null) {
 			return null
 		}
+
 		let current_node = node.right
+
 		while (current_node.is_leaf == false) {
 			current_node = current_node.left
 		}
 
 		return current_node
 	}
-	
-	/*
-	* returns the x coordinate of the intersection of the two parabolas
-	*/
-	get_x_of_edge(node, directrix) {
-		let left = this.get_first_leaf_on_left(node)
-		let right = this.get_first_leaf_on_right(node)
 
-		
-		let p = left.site
-		let r = right.site
+	/** @function
+	 * @returns {number} x - x coordinate of the intersection of the two parabolas
+	  */
+	get_x_of_edge(node, directrix) {
+		const left = this.get_first_leaf_on_left(node)
+		const right = this.get_first_leaf_on_right(node)
+
+		const p = left.site
+		const r = right.site
 
 		console.log(left, right)
 
@@ -157,7 +152,6 @@ class BeachTree {
 		const b1 = -2 * p.x / dp
 		const c1 = directrix + dp / 4 + p.x * p.x / dp
 
-		
 		dp = 2 * (r.y - directrix)
 		const a2 = 1 / dp
 		const b2 = -2 * r.x / dp
@@ -166,30 +160,35 @@ class BeachTree {
 		const a = a1 - a2
 		const b = b1 - b2
 		const c = c1 - c2
-		
 
 		const disc = b * b - 4 * a * c
 		const x1 = (-b + Math.sqrt(disc)) / (2 * a)
 		const x2 = (-b - Math.sqrt(disc)) / (2 * a)
 
-
 		if (p.y < r.y) {
 			return Math.max(x1, x2)
-		} else {
+		}
+
+		else {
 			return Math.min(x1, x2)
 		}
 	}
-	
+
 	find_arc_above(site) {
 		let current_node = this.root
+
 		while (current_node.is_leaf == false) {
-			let x = this.get_x_of_edge(current_node, site.y)
-			if( x > site.x) {
+			const x = this.get_x_of_edge(current_node, site.y)
+
+			if (x > site.x) {
 				current_node = current_node.left
-			} else {
+			}
+
+			else {
 				current_node = current_node.right
 			}
 		}
+
 		return current_node
 	}
-} 
+}
