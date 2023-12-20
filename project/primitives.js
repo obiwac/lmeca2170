0b9ac7395020891bfe514a411d52342de9dd3cec
obiwac <obiwac@gmail.com>
2023-12-20 19:11:47 +0100

    project: `Line` primitive

--- project/primitives.js
+++ project/primitives.js
@@ -45,3 +45,18 @@ class Triangle {
 		this.c = c
 	}
 }
+
+class Line {
+	/** @function
+	  * @param {number} x1
+	  * @param {number} y1
+	  * @param {number} x2
+	  * @param {number} y2
+	  */
+	constructor(x1, y1, x2, y2) {
+		this.x1 = x1
+		this.y1 = y1
+		this.x2 = x2
+		this.y2 = y2
+	}
+}
